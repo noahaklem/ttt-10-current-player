1fe81ed8e7b3a2efcaeb4879b80204d786de9cec
Noah A Klem <noahaklem@gmail.com>
2023-09-02 19:21:17 -0600

    current_player method

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,7 @@
 def turn_count(board)
-    counter = 0
-    board.each {|player| player == "X" || player = "O" ? counter += 1 }
-    return counter
+   board.count {|token| token == "X" || token == "O"}
+end
+
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
 end
